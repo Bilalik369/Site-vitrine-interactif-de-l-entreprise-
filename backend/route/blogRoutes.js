32ad38d22617b29aa83f0e40c1688089a644ea0a
Bilalik369 <bilal11iken@gmail.com>
2025-07-29 12:13:35 +0100

    ajoute la fonction pour get un article

--- backend/route/blogRoutes.js
+++ backend/route/blogRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {createArticle , getAllArticle} from "../controllers/blogController.js"
+import {createArticle , getAllArticle, getAllArticleById} from "../controllers/blogController.js"
 
 const router = express.Router();
 
 router.post("/" , createArticle)
 router.get("/" , getAllArticle)
+router.get("/:id" , getAllArticleById)
 
 export default router
